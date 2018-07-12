1ff62e722bd874afad5d50a45e2f97846bbbee52
Magfiozy777 <34837894+Magfiozy777@users.noreply.github.com>
2018-07-12 13:18:31 +0300

    adding web app

---
+++ Doroga.Web/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+
+namespace HelloAngularApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                // webpack
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
---
+++ Doroga.Web/src/main.ts
@@ -0,0 +1,16 @@
+﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
+
+//reload the app when you changed code
+if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => {
+        // create a new 'app' before start
+        const oldRootElem = document.querySelector('app');
+        const newRootElem = document.createElement('app');
+        oldRootElem!.parentNode!.insertBefore(newRootElem, oldRootElem);
+        platform.destroy();
+    });
+}
